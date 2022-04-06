f55b2e3bd1427b40ceeb385fc350abd1a0aaf08d
Igor <brajkovich.igor@gmail.com>
2022-04-06 09:24:41 +0200

    nove cijene bike dodane

--- en/php/services-bike.php
+++ en/php/services-bike.php
@@ -267,78 +267,9 @@
 
                             </tr>
                         </thead>
-                        <tbody>
-                            <tr>
-                                <td>1H - 20,00kn</td>
-                                <td>1H - 30,00kn</td>
-
-
-                            </tr>
-                            <tr>
-                                <td>4H - 50,00kn</td>
-                                <td>4H - 70,00kn</td>
-                                <td>4H - 200,00kn</td>
-
-                            </tr>
-                            <tr>
-                                <td>1 Day - 80,00kn</td>
-                                <td>1 Day - 100,00kn</td>
-                                <td>1 Day - 300,00kn</td>
-                            </tr>
-                            <tr>
-                                <td>2 Days - 150,00kn</td>
-                                <td>2 Days - 190,00kn</td>
-                                <td>2 Days - 570,00kn</td>
-                            </tr>
-                            <tr>
-                                <td>3 Days - 210,00kn</td>
-                                <td>3 Days - 270,00kn</td>
-                                <td>3 Days - 830,00kn</td>
-                            </tr>
-                            <tr>
-                                <td>4 Days - 260,00kn</td>
-                                <td>4 Days - 320,00kn</td>
-                                <td>4 Days - 1060,00kn</td>
-                            </tr>
-                            <tr>
-                                <td>5 Days - 300,00kn</td>
-                                <td>5 Days - 400,00kn</td>
-                                <td>5 Days - 1280,00kn</td>
-                            </tr>
-                            <tr>
-                                <td>6 Days - 350,00kn</td>
-                                <td>6 Days - 450,00kn</td>
-                                <td>6 Days - 1500,00kn</td>
-                            </tr>
-                            <tr>
-                                <td>7 Days - 400,00kn</td>
-                                <td>7 Days - 500,00kn</td>
-                                <td>7 Days - 1650,00kn</td>
-                            </tr>
-                        </tbody>
+                        <?php include "../var-cjene/bike-cjene.php"; ?>
                     </table>
-                </div>
-
-                <div class="table-responsive">
-                    <table class="table table-hover table-striped">
-                        <thead>
-                            <tr>
-                                <th scope="col">BACK PACK</th>
-                                <th scope="col">BACK PACK WITH TOOLS,PUMP & FIRST AID</th>
-                                <th scope="col">SERVICE 1H</th>
-
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td>20.00 kn</td>
-                                <td>50,00 kn</td>
-                                <td>125,00 kn</td>
-
-                            </tr>
-
-                        </tbody>
-                    </table>
+ 
                 </div>
 
                 <hr>
